a80566199e30a9b1636c0253bee027b1d4934dfb
syncodd <m.emrearslan595@gmail.com>
2021-11-30 21:00:38 +0300

    test

--- routes/todo.go
+++ routes/todo.go
@@ -8,7 +8,7 @@ import (
 func TodoRoute(route fiber.Router) {
 	route.Get("", controller.GetTodos)
 	route.Post("", controller.CreateTodo)
-	route.Put("/:id", controller.UpdateTodo)
+	route.Put("/:id", controller.UpdateTodo) // curl -i -X PUT -H "Content-Type: application/json" -d "{""title"":""saamet""}" 127.0.0.1:8000/api/todos/1
 	route.Delete("/:id", controller.DeleteTodo)
 	route.Get("/:id", controller.GetTodo)
 }
